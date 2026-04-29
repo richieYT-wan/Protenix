1e19d42251f2dc401feff3ce7c3dced63a06e57a
Yat <richieytwan@yahoo.fr>
2026-04-29 10:36:28 +0000

    Added dummy MSA generation and target MSA precomputation in script

--- scripts/make_json_from_csv.py
+++ scripts/make_json_from_csv.py
@@ -1,9 +1,17 @@
+"""
+
+This
+
+"""
+
 from argparse import ArgumentParser
 import pandas as pd
 import json
 import os, sys
 from pathlib import Path
-from typing import List, Union
+from typing import List, Union, Dict, Tuple
+import subprocess
+import tempfile
 
 def parse_args():
     parser = ArgumentParser()
@@ -17,7 +25,8 @@ def parse_args():
     parser.add_argument('--pairing_db', type=str, default= None, help='ex: uniref100')
     return parser.parse_args()
 
-def find_target_col(df, candidates=None):
+
+def find_target_col(df: pd.DataFrame, candidates: List[str]= None):
     """
     Find the column in a DataFrame that likely contains target sequences.
 
@@ -59,26 +68,84 @@ def find_target_col(df, candidates=None):
     )
 
 
-def make_msa(precomputed_msa_dir=None, pairing_db='uniref100'):
-    res = {'pairing_db': pairing_db}
-    if precomputed_msa_dir:
-        res['precomputed_msa_dir'] = precomputed_msa_dir
-    return res
+def process_target_msa_template(target: Union[List, str], output_dir: Path):
+    """
+    Takes the target and builds a JSON to then run `protenix mt` and get its MSA and template
+    Args:
+        target:
+
+    Returns:
+    """
+
+    protein_chains = []
+    if type(target)==list:
+        for i, t in enumerate(target):
+            protein_chains.append(make_protein_chain(t, f'T{i}', 1, None, None))
+    elif type(target)==str:
+        protein_chains.append(make_protein_chain(target, 'T', 1, None, None))
+
+    entry = [make_entry(protein_chains, 'target', None, None)]
+    with tempfile.NamedTemporaryFile('w', suffix='.json', delete=False) as tmp:
+        json.dump(entry, tmp, indent=2)
+        tmp_path = Path(tmp.name)
+
+    _ = subprocess.run(
+        [
+            "protenix", "mt",
+            "-i", str(tmp_path),
+            "-o", str(output_dir),
+        ],
+        text=True,
+        check=True
+    )
+
+    target_file = tmp_path.parent / (tmp_path.stem + '-final-updated.json')
+    with open(target_file, 'r') as f:
+        target_json = json.load(f)
+
+    return target_json
+
+def make_msa(pairing_path: Path=None, nonpairing_path: Path=None):
+    # Do default old behaviour
+    if not (pairing_path and nonpairing_path):
+        return {'pairing_db': 'uniref100'}
+    else:
+        return {'pairedMsaPath': str(pairing_path),
+                'unpairedMsaPath': str(nonpairing_path)}
+
+def make_dummy_msa(sequence: str, name: str, msa_dir: Path) -> Tuple[Path, Path]:
+    """
+    Creates a dummy MSA for a given sequence
+    msa_dir should be the containing all the msas, e.g. path/to/msa
+    then in this dir there will be path/to/msa/name/pairing.a3m, msa/<name>/non_pairing.a3m etc
+    """
+    os.makedirs(msa_dir / name, exist_ok=True)
+    pairing_path = msa_dir / name / 'pairing.a3m'
+    nonpairing_path = msa_dir / name / 'non_pairing.a3m'
+    with open(msa_dir / name / 'pairing.a3m', 'w') as f:
+        f.writelines(f'>{name}\n{sequence}\n')
+    with open(msa_dir / name / 'non_pairing.a3m', 'w') as f:
+        f.writelines(f'>{name}\n{sequence}\n')
+
+    return pairing_path, nonpairing_path
+
 
 def make_protein_chain(sequence, id, count=1,
-                       precomputed_msa_dir=None, pairing_db='uniref100'):
+                       pairing_path: Union[str,Path]=None,
+                       nonpairing_path: Union[str, Path]=None):
     return {
         "proteinChain": {
             "sequence": sequence,
             "id": [id],
             "count": count,
-            "msa": make_msa(precomputed_msa_dir, pairing_db)
+            "msa": make_msa(pairing_path, nonpairing_path)
         }
     }
 
 # Placeholder for if we add constraints to the folding
 def make_contraints():
     pass
+
 
 def make_entry(protein_chains, name, seeds=None, constraints=None):
     entry = {"name": name}
@@ -91,13 +158,38 @@ def make_entry(protein_chains, name, seeds=None, constraints=None):
     return entry
 
 
-def make_entry_from_row(vh:str, target: Union[List, str], name: str, seeds=None,
+def make_entry_from_row(vh:str, target_json:Dict, name: str, msa_dir: Path,
+                        seeds: List[int]=None, constraints: List[str]=None,
+                        ):
+    """
+    Used to create a full entry based on iterating on vh sequences
+    Args:
+        vh:
+        target: Dictionary as processed by process_target_msa_template
+        name:
+        seeds:
+        precomputed_msa_dir:
+        pairing_db:
+        constraints:
+
+    Returns:
+        entry
+    """
+    # Start with VH with dummy MSA
+    pairing_path, non_pairing_path = make_dummy_msa(vh, name, msa_dir)
+    protein_chains = [make_protein_chain(vh, 'H', 1, pairing_path, non_pairing_path)]
+    protein_chains.extend(target_json[0]['sequences'])
+    return make_entry(protein_chains, name, seeds, constraints)
+
+
+
+def make_entry_from_old(vh:str, target:Dict, name: str, seeds=None,
                         precomputed_msa_dir=None, pairing_db=None, constraints=None):
     """
     Used to create a full entry based on iterating on vh sequences
     Args:
         vh:
-        target:
+        target: Dictionary as processed by process_target_msa_template
         name:
         seeds:
         precomputed_msa_dir:
@@ -119,16 +211,9 @@ def make_entry_from_row(vh:str, target: Union[List, str], name: str, seeds=None,
 
 def main():
     args = parse_args()
+    # Read the df and parse rows to be selected + creates outfile
     start, end = args.rows
     df = pd.read_csv(args.input_file).iloc[start:end]
-    # If not target is provided will try to parse from the df
-    if args.target_sequences is None:
-        target_col = find_target_col(df)
-        # assumes a single target for every sequence in the df
-        target = df[target_col].unique()[0]
-    else:
-        target = args.target_sequences
-
     # If no output_file specified, will save in the same directory as input_file with filename replaced as json
     if not args.output_file:
         parent = args.input_file.parent
@@ -137,16 +222,33 @@ def main():
     else:
         outfile = args.out_file
 
+
+    ###################################################
+    #    Processing target for MSA/template search    #
+    ###################################################
+    # If no target is provided will try to parse from the df
+    if args.target_sequences is None:
+        target_col = find_target_col(df)
+        # assumes a single target for every sequence in the df
+        target = df[target_col].unique()[0]
+    else:
+        target = args.target_sequences
+
+    target_json = process_target_msa_template(target, outfile.parent)
+
+    ###################################################
+    #      Processing VHH + target for final JSON     #
+    ###################################################
     df['seq_id'] = [f'{args.input_file.stem}_id_{i:06}' for i in range(len(df))]
     entries = []
     for _, row in df[['vh', 'seq_id']].iterrows():
-        entries.append(make_entry_from_row(row['vh'], target,
+        entries.append(make_entry_from_row(row['vh'], target_json,
                                            name = row['seq_id'], seeds=args.seeds,
-                                           precomputed_msa_dir=args.precomputed_msa_dir,
-                                           pairing_db=args.pairing_db, constraints=None))
+                                           msa_dir = outfile.parent / 'msa', constraints=None))
 
-    with open(outfile, 'w') as f:
-        json.dump(entries, f, indent=4)
+    with open(outfile, 'w') as file:
+        json.dump(entries, file, indent=2)
+
 
 if __name__=='__main__':
     main()
